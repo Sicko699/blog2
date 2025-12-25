f1cbef980afa7f7c36bada00b80b4bd66c8d9891
Sicko699 <francesco.mattone.03@gmail.com>
2025-12-25 16:17:29 +0100

    cambio urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://tuodominio.it", // <-- IMPORTANTISSIMO per canonical+sitemap
+  site: "https://blog2-ci6.pages.dev", // <-- IMPORTANTISSIMO per canonical+sitemap
   integrations: [sitemap()],
 });
